03c14524299e7c7395d08a406bc7785862d246e5
Ismael León Cal <ileoncal@gmail.com>
2024-10-24 15:19:54 +0200

    Ejercicio 8 Done

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -4,14 +4,21 @@ public class Exercise08 {
 
     public static void main(String[] args) {
 
-    }
+        int Y= 10;//Defino el valor de Y
 
+        whileLoop(Y);//Llamo al metodo
+
+    }
     //TODO ↓
     // Comprueba el funcionamiento del bucle while, que para en cada iteración muestre el siguiente mensaje:
     // X < Y. El próximo ciclo valdrá: X+1
     // X+1 < Y. El próximo ciclo valdrá: X+2
     // etc.
-    public static void whileLoop(int num) {
-
+    public static void whileLoop(int Y) {
+        int X=0; //Empiezo el loop en 0
+        while (X < Y){
+            System.out.println(X + " < " + Y + ". El proximo ciclo valdra: " + (X+1));
+            X++;//Sumo 1 al valor de X
+        }
     }
 }
